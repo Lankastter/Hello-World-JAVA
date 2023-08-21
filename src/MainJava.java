77e4985a4a0c20e8e083ee56684d213dd16f941c
VICTOR LANKASTER <lankastersilva@gmail.com>
2023-08-20 22:08:26 -0300

    commit

--- src/MainJava.java
+++ src/MainJava.java
@@ -1,5 +1,8 @@
 public class MainJava{
     public static void main(String[]args){
         System.out.println("Olá Mundooo!");
+        System.out.println("Criando outra linha");
+        System.out.println("Vamos quebrar essa linha \nVamos" +
+                " quebrar essa linha tmb \nok, ok");
     }
 }
